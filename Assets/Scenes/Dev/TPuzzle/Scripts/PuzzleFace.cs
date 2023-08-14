591c2ae5587abf0436a7856e4c48c85b39effbc7
Fao2212 <ferolsen@hotmail.com>
2023-08-14 10:28:28 -0600

    Se agregan escenas para trabajar en el puzzle T
    Se crean escenas para pruebas y para trabajar en crear una figura T. Se va a hacer un sistema de emparejamiento de caras de figuras geometricas.

---
+++ Assets/Scenes/Dev/TPuzzle/Scripts/PuzzleFace.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//It works in editor mode.
+public class PuzzleFace : MonoBehaviour
+{
+    //From origin create 4 colliders.
+    //Colliders will have a width and a height from the origin point(parent).
+    //Collider size will be defined by a parameter. .1 by default.
+    //Each collider will have a target to attach to. I can make that Vertex 1 ALWAYS have to collide with vertex 1 from the other part.
+}
