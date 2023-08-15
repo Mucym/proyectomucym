4f1e1ffda40a52e3c2fc747b3a4f7b783c003fdb
Fao2212 <ferolsen@hotmail.com>
2023-08-14 20:34:36 -0600

    Se crean la descripcion de las funciones para el sistema
    Funciones que permiten que el editor cambie el estado de los cubos.

--- Assets/Scenes/Dev/TPuzzle/Scripts/PuzzleFace.cs
+++ Assets/Scenes/Dev/TPuzzle/Scripts/PuzzleFace.cs
@@ -5,8 +5,34 @@ using UnityEngine;
 //It works in editor mode.
 public class PuzzleFace : MonoBehaviour
 {
+    public float width = 1f;
+    public float height = 1f;
+    public float colliderScale = .1f;
+    public PuzzleFace targetFace;
+
     //From origin create 4 colliders.
     //Colliders will have a width and a height from the origin point(parent).
     //Collider size will be defined by a parameter. .1 by default.
     //Each collider will have a target to attach to. I can make that Vertex 1 ALWAYS have to collide with vertex 1 from the other part.
+
+    //Triggered when width or height is changed in the editor.
+    public void SetDimensions()
+    {
+
+    }
+
+    //Triggered when the size is changed in the editor.
+    public void ChageSize()
+    {
+
+    }
+
+    //Used to check if the face is in the correct position with the targetFace
+    public void CheckCollisions()
+    {
+
+    }
+
+   
+
 }
