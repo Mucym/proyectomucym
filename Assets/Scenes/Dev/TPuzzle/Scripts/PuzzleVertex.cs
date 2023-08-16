db7b0ed84e551eefac97cfcf6c1f6f1b07f26375
Fao2212 <ferolsen@hotmail.com>
2023-08-15 19:29:49 -0600

    Se termina el sistema de creacion de caras.
    Se tiene un sistema que crea y asigna una cara target para comprobar si se posisionaron de manera correcta. Estas caras son editables y cambian en el editor. Es necesario hacer un test, para comprobar que funcionan bien.

--- Assets/Scenes/Dev/TPuzzle/Scripts/PuzzleVertex.cs
+++ Assets/Scenes/Dev/TPuzzle/Scripts/PuzzleVertex.cs
@@ -2,17 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Positions { TopRight,TopLeft,BottomRight,BottomLeft}
+
 public class PuzzleVertex : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Positions position;
+    public PuzzleVertex targetVertex;
+    public bool collidingWithTargetVertex;
+
+    public void AdjustPosition(float width,float height,Vector3 parentPosition)
     {
-        
+        transform.position = parentPosition;
+        switch (position)
+        {
+            case Positions.TopRight:
+                transform.position = transform.position + new Vector3(width / 2, 0 ,-height/2);
+                break;
+            case Positions.TopLeft:
+                transform.position = transform.position + new Vector3(-width / 2, 0, -height / 2);
+                break;
+            case Positions.BottomRight:
+                transform.position = transform.position + new Vector3(width / 2, 0, height / 2);
+                break;
+            case Positions.BottomLeft:
+                transform.position = transform.position + new Vector3(-width / 2, 0, height / 2);
+                break;
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AdjustSize(float size)
     {
-        
+        transform.localScale = new Vector3(size,size,size);
+    }
+
+    public void SetTargetVertex(PuzzleVertex[] vertices)
+    {
+        if(vertices != null)
+        {
+            foreach (PuzzleVertex vertex in vertices)
+            {
+                if (vertex.position == position)
+                {
+                    targetVertex = vertex;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == targetVertex.gameObject)
+        {
+            collidingWithTargetVertex = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) 
+    {
+        if (collision.gameObject == targetVertex.gameObject)
+        {
+            collidingWithTargetVertex = false;
+        }
     }
 }
